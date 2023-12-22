4885321fbbe61d5462f00719f28556fa5924ec16
Konig Koudogbo <koudogbokonig@gmail.com>
2023-12-22 17:06:04 +0100

    Create listener

---
+++ search-api/src/main/java/fr/lernejo/search/api/ElasticSearchConfiguration.java
@@ -0,0 +1,60 @@
+package fr.lernejo.search.api;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfiguration {
+
+    @Value("${elasticsearch.host:localhost}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.port:9200}")
+    private int elasticsearchPort;
+
+    @Value("${elasticsearch.username:elastic}")
+    private String elasticsearchUsername;
+
+    @Value("${elasticsearch.password:admin}")
+    private String elasticsearchPassword;
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient(
+        @Value("${elasticsearch.host:localhost}") String host,
+        @Value("${elasticsearch.port:9200}") int port,
+        @Value("${elasticsearch.username:elastic}") String username,
+        @Value("${elasticsearch.password:admin}") String password) {
+
+        // Configurer et renvoyer le client ElasticSearch
+        RestClientBuilder restClientBuilder = RestClient.builder(
+            new HttpHost(host, port, "http")
+        );
+
+        restClientBuilder.setHttpClientConfigCallback(
+            httpClientBuilder -> httpClientBuilder
+                .setDefaultCredentialsProvider(createCredentialsProvider(username, password))
+        );
+
+        return new RestHighLevelClient(restClientBuilder);
+    }
+
+    private CredentialsProvider createCredentialsProvider(String username, String password) {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+            AuthScope.ANY,
+            new UsernamePasswordCredentials(username, password)
+        );
+        return credentialsProvider;
+    }
+}
+
+
---
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -0,0 +1,58 @@
+package fr.lernejo.search.api;
+
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.xcontent.XContentType;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.util.Map;
+
+@Component
+public class GameInfoListener {
+
+    private final RestHighLevelClient restHighLevelClient;
+
+    @Autowired
+    public GameInfoListener(RestHighLevelClient restHighLevelClient) {
+        this.restHighLevelClient = restHighLevelClient;
+    }
+
+    @RabbitListener(queues = AmqpConfiguration.GAME_INFO_QUEUE)
+    public void onMessage(String message, @Header("game_id") String gameId, Message amqpMessage) {
+        try {
+            // Accessing message properties
+            Map<String, Object> headers = amqpMessage.getMessageProperties().getHeaders();
+            String contentType = headers.get("content_type").toString();
+
+            // Index the document in the 'games' index with the provided game_id and content_type
+            IndexRequest indexRequest = new IndexRequest("games")
+                .id(gameId)
+                .source(message, XContentType.JSON);
+                // .setPipeline(contentType);  // Assuming content_type is a valid pipeline name
+            
+
+            IndexResponse response = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+
+            // Handle the response if needed
+            // response.getResult();  // Get the result of the index operation
+            // Handle the response if needed
+            if (response.getResult() == DocWriteResponse.Result.CREATED) {
+                System.out.println("Document created successfully with ID: " + response.getId());
+            } else if (response.getResult() == DocWriteResponse.Result.UPDATED) {
+                System.out.println("Document updated successfully with ID: " + response.getId());
+            }
+
+        } catch (Exception e) {
+            // Handle exceptions (e.g., log or rethrow)
+            e.printStackTrace();
+        }
+    }
+
+}
